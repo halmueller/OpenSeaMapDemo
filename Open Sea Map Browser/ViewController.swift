55d8a4cec6f686c4269f0bc0bae4bc23ef7911bf
Hal Mueller <hal@mobilegeographics.com>
2015-08-05 12:58:16 -0700

    Initial Commit

---
+++ Open Sea Map Browser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Open Sea Map Browser
+//
+//  Created by Hal Mueller on 8/5/15.
+//  Copyright (c) 2015 Hal Mueller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
