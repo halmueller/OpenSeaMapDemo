4996c93fb3402f263eaf09d777a96402cdedeff7
Hal Mueller <hal@mobilegeographics.com>
2015-08-05 14:11:36 -0700

    basic Open Sea Map working

--- Open Sea Map Browser/ViewController.swift
+++ Open Sea Map Browser/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let openSeaMapOverlay = MKTileOverlay(URLTemplate:"http://tiles.openseamap.org/seamark/{z}/{x}/{y}.png")
+        openSeaMapOverlay.minimumZ = 8
+        openSeaMapOverlay.maximumZ = 16
+        self.mapView.addOverlay(openSeaMapOverlay, level: MKOverlayLevel.AboveRoads)
+
+        let demoCenter = CLLocationCoordinate2D(latitude: 54.19, longitude: 12.09)
+        let demoRegion = MKCoordinateRegionMake(demoCenter, MKCoordinateSpanMake(0.015, 0.028))
+        self.mapView.setRegion(demoRegion, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        if overlay is MKTileOverlay {
+            return MKTileOverlayRenderer(overlay: overlay)
+        }
+        NSLog("no renderer found")
+        return nil
+    }
 }
 
