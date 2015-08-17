bfe957ed9350c521c1d8ee9d2a645eb6c5df224a
Hal Mueller <hmueller@uw.edu>
2015-08-17 10:13:06 -0700

    remove some meaningless whitespace to bump the build number

--- Open Sea Map Browser/AppDelegate.swift
+++ Open Sea Map Browser/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         NSUserDefaults.standardUserDefaults().registerDefaults([useOpenSeaMapKeystring: true, mapStyleKeystring: 0])
         return true
@@ -40,7 +39,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
 
 }
 
